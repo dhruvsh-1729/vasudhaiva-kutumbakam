8f22c52280d0ff0d44ebaf810b738c846138aa6c
Dhruv Shah <78747805+dhruvsh-1729@users.noreply.github.com>
2025-10-24 17:26:22 +0530

    Update redirect URL in NotificationBanner component

--- components/NotificationBanner.tsx
+++ components/NotificationBanner.tsx
@@ -5,7 +5,7 @@ const NotificationBanner: React.FC = () => {
   const [isVisible, setIsVisible] = useState<boolean>(true);
 
   const handleRedirect = (): void => {
-    window.open('https://vk.jyot.in', '_blank');
+    window.open('https://vk.jyot.in/12-guarantees', '_blank');
   };
 
   const handleDismiss = (): void => {
